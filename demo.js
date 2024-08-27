c6a97b3c01fb490f0b14d0e1b8168ebdf4d413c9
HARISH23K3 <hariyanagandula@gmail.com>
2024-08-27 12:27:38 +0530

    second commit

---
+++ demo.js
@@ -0,0 +1,18 @@
+var a = [1, 2, 3, "Ram", "Sitha"];
+for (let i = 0; i < a.length; i++) {
+  console.log(a[i]);
+}
+function welcome1() {
+  alert("Welcome to our page");
+}
+// n = prompt("Enter the value 2 + 3 =");
+// if (n == 5) {
+//   window.alert("Correct answer");
+// } else {
+//   window.alert("Wrong answer");
+// }
+function welcome2() {
+  var a = document.getElementById("h1");
+  // console.log(a.innerHTML);
+  a.innerHTML = "you are welcomed";
+}
